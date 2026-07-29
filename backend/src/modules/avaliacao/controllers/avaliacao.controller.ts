1bf58d018aa8d5de8906ce7fe0e9b2b07d5589af
Rubens Matheus <y2afepolfxx@gmail.com>
2026-07-29 14:46:36 -0300

    feat(IS-86): endpoint deletar Avaliacao

--- backend/src/modules/avaliacao/controllers/avaliacao.controller.ts
+++ backend/src/modules/avaliacao/controllers/avaliacao.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Param,
+  ParseUUIDPipe,
+  Post,
+} from '@nestjs/common';
+import { CreateAvaliacaoDto } from '../dto/create-avaliacao.dto';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { CreateAvaliacaoCommand } from '../commands/create-avaliacao/create-avaliacao.command';
+import { DeleteAvaliacaoCommand } from '../commands/delete-avaliacao/delete-avaliacao.command';
 
 @Controller('avaliacoes')
-export class AvaliacaoController {}
+export class AvaliacaoController {
+  constructor(
+    private readonly queryBus: QueryBus,
+    private readonly commandBus: CommandBus,
+  ) {}
+
+  @Post()
+  create(@Body() avaliacao: CreateAvaliacaoDto) {
+    return this.commandBus.execute(new CreateAvaliacaoCommand(avaliacao));
+  }
+
+  @Delete(':id')
+  delete(@Param('id', ParseUUIDPipe) id: string) {
+    return this.commandBus.execute(new DeleteAvaliacaoCommand(id));
+  }
+}
